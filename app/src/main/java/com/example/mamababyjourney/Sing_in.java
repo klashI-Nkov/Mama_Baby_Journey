9a361d01f966f6477e9f8587806731c02943c550
klashinkov <klash1200@gmail.com>
2023-04-14 17:57:24 +0300

    By Mohammed issa in 14/4/23 At 5:22 PM

--- app/src/main/java/com/example/mamababyjourney/Sing_in.java
+++ app/src/main/java/com/example/mamababyjourney/Sing_in.java
@@ -1,4 +1,4 @@
-package com.example.mamababyjourney;
+package com . example . mamababyjourney ;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,113 +7,61 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.WindowManager;
+
 import com.example.mamababyjourney.databinding.ActivitySingInBinding;
 
 import java.util.Objects;
 
 public class Sing_in extends AppCompatActivity
 {
-    ActivitySingInBinding binding;
-    int x = 1 ;
+    ActivitySingInBinding binding ;
+
     @Override
     protected void onCreate ( Bundle savedInstanceState )
     {
-        getWindow ( ) . setFlags ( WindowManager. LayoutParams . FLAG_FULLSCREEN , WindowManager . LayoutParams . FLAG_FULLSCREEN  ) ;
-        Objects.requireNonNull ( getSupportActionBar ( ) ).hide();
+        getWindow ( ) . setFlags ( WindowManager . LayoutParams . FLAG_FULLSCREEN , WindowManager . LayoutParams . FLAG_FULLSCREEN ) ;
+        Objects . requireNonNull ( getSupportActionBar ( ) ) . hide ( ) ;
 
-        super.onCreate ( savedInstanceState );
-        binding = ActivitySingInBinding . inflate ( getLayoutInflater ( ) );
-        setContentView ( binding . getRoot ( ) );
+        super . onCreate ( savedInstanceState ) ;
+        binding = ActivitySingInBinding . inflate ( getLayoutInflater ( ) ) ;
+        setContentView ( binding . getRoot ( ) ) ;
 
         binding . EmailEditText . addTextChangedListener ( watcher ) ;
         binding . passwordEditText . addTextChangedListener ( watcher ) ;
-        /*binding.BTN.setOnClickListener ( v ->
-        {
 
-            if (x==7)
-                x=1;
-
-            switch ( x )
-            {
-                case 1:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c1 ) );
-                    binding.textView5.setText ( "1" );
-                    break;
-                }
-
-                case 2:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c2 ) );
-                    binding.textView5.setText ( "2" );
-                    break;
-                }
-
-                case 3:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c3 ) );
-                    binding.textView5.setText ( "3" );
-                    break;
-                }
-
-                case 4:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c4 ) );
-                    binding.textView5.setText ( "4" );
-                    break;
-                }
-
-                case 5:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c5 ) );
-                    binding.textView5.setText ( "5" );
-                    break;
-                }
-
-                case 6:
-                {
-                    binding.con.setBackgroundColor (  getResources () .getColor ( R.color.c6 ) );
-                    binding.textView5.setText ( "6" );
-                    break;
-                }
-
-            }
-
-            x++;
-
-        } );*/
-
-        binding.PassFloatHint.setVisibility ( View.INVISIBLE );
-        binding.EmailFloatHint.setVisibility ( View.INVISIBLE );
+        binding . PassFloatHint . setVisibility ( View . INVISIBLE ) ;
+        binding . EmailFloatHint . setVisibility ( View . INVISIBLE ) ;
 
     }
 
     TextWatcher watcher = new TextWatcher ( )
     {
         @Override
-        public void onTextChanged ( CharSequence s,int start,int before,int count )
+        public void onTextChanged ( CharSequence s , int start , int before , int count )
         {
-            if ( ! ( binding.EmailEditText.getText ().toString ().isEmpty () ) )
-                binding.EmailFloatHint.setVisibility ( View.VISIBLE );
+            if ( !( binding . EmailEditText . getText ( ) . toString ( ) . isEmpty ( ) ) )
+            { binding . EmailFloatHint . setVisibility ( View . VISIBLE ) ; }
             else
-                binding.EmailFloatHint.setVisibility ( View.INVISIBLE );
+            { binding . EmailFloatHint . setVisibility ( View . INVISIBLE ) ; }
 
-            if ( ! ( binding.passwordEditText.getText ().toString ().isEmpty () ) )
-                binding.PassFloatHint.setVisibility ( View.VISIBLE );
+            if ( !( binding . passwordEditText . getText ( ) . toString ( ) . isEmpty ( ) ) )
+            { binding . PassFloatHint . setVisibility ( View . VISIBLE ) ; }
             else
-                binding.PassFloatHint.setVisibility ( View.INVISIBLE );
+            { binding . PassFloatHint . setVisibility ( View . INVISIBLE ) ; }
 
         }
 
         @Override
-        public void beforeTextChanged ( CharSequence s,int start,int count,int after ){
+        public void beforeTextChanged ( CharSequence s , int start , int count , int after )
+        {
 
         }
 
         @Override
-        public void afterTextChanged ( Editable s ){
+        public void afterTextChanged ( Editable s )
+        {
 
         }
-    } ;
+    };
 
 }
