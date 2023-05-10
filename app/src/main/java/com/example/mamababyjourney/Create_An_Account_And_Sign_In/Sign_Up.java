dc100f420f5f2b6b3a1a15df72a69d41c3d9efb0
klashinkov <klash1200@gmail.com>
2023-05-10 07:10:30 +0300

    By mohammed issa in 10/5/23 AT 7:10 AM

--- app/src/main/java/com/example/mamababyjourney/Create_An_Account_And_Sign_In/Sign_Up.java
+++ app/src/main/java/com/example/mamababyjourney/Create_An_Account_And_Sign_In/Sign_Up.java
@@ -1,17 +1,20 @@
 package com.example.mamababyjourney.Create_An_Account_And_Sign_In;
 
-import com.example.mamababyjourney.R;
 import com.example.mamababyjourney.databinding.ActivitySignUpBinding;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.res.Configuration;
-import android . view    . WindowManager;
-import java    . util    . Objects;
-import android . content . Intent;
-import android . os      . Bundle;
-import android . view    . View;
+import android.annotation.SuppressLint;
+import com.example.mamababyjourney.R;
+import androidx.annotation.NonNull;
+import android.view.WindowManager;
+import android.content.Intent;
+import android.widget.Toast;
+import java.util.Objects;
+import android.os.Bundle;
+import android.view.View;
+
+@SuppressWarnings ( { "StatementWithEmptyBody" , "CommentedOutCode" , "deprecation" } )
+@SuppressLint ( { "WrongConstant" , "ShowToast" } )
 
 public class Sign_Up extends AppCompatActivity
 {
@@ -22,34 +25,89 @@ public class Sign_Up extends AppCompatActivity
     protected void onCreate ( Bundle savedInstanceState )
     {
         getWindow ( ) . setFlags ( WindowManager . LayoutParams . FLAG_LAYOUT_NO_LIMITS , WindowManager . LayoutParams . FLAG_LAYOUT_NO_LIMITS ) ;
-        Objects. requireNonNull ( getSupportActionBar ( ) ) . hide ( ) ;
+        Objects . requireNonNull ( getSupportActionBar ( ) ) . hide ( ) ;
 
         super . onCreate ( savedInstanceState ) ;
         binding = ActivitySignUpBinding . inflate ( getLayoutInflater ( ) ) ;
         setContentView ( binding . getRoot ( ) ) ;
     }
 
+    // هاد الفنكشن مربوط بزر انشاء الحساب و وظفيته انه ينقل المستخدم للشاشه الي بعد شاشة انشاء الحساب
     public void Go_to_Info ( View view )
     {
-        Intent intent = new Intent ( this , Info_Activity . class ) ;
-        startActivity ( intent ) ;
+        /*
+            هون في حالة المستخدم لما يسجل باستعمال الايميل كان ام او طبيب لو ما حدد صفته يعطل زر انشاء الحساب و ما يخليه قادر يكمل للشاشه الي
+            بعدها وبعرض اله مسج انه لازم يحدد صفته قبل ما يكمل اما اذا كان محدد فعادي بخليه يكمل من دون اي مشاكل
+         */
+        if ( !binding . MomRBTN2 . isChecked ( ) && !binding . DoctorRBTN2 . isChecked ( ) )
+        {
+            binding . singUpBTN . setEnabled ( false ) ;
+            Toast . makeText ( this , "يرجى تحديد صفتك قبل الانتقال الي  الصفحه التاليه" , Toast . LENGTH_LONG ) . show ( ) ;
+        }
+        else
+        { binding . singUpBTN . setEnabled ( true ) ; }
+
+
+        // هون كونه بس الام مطلوب منها الاسم بس فما رح يكون في الها بيانات تعبيها لهيك حاكي اله اذا المستخدم كان دكتور انقله لشاشة البيانات عشان يعبي البيانات اللازمه
+        if ( binding . DoctorRBTN2 . isChecked ( ) )
+        {
+            Intent intent = new Intent ( this , Info_Activity . class ) ;
+            startActivity ( intent ) ;
+        }
+
     }
 
+    // هاد الفنكشن عمله عشان اغير لون التكست لل Radio buttons حسب الثيم لانه حاولت اطبق عليهم ستايل يغيير لون الخط حسب الثيم ما زبط فانجبرت استعمل هاي الطريقه
     @Override
     public void onConfigurationChanged ( @NonNull Configuration newConfig )
     {
-        super.onConfigurationChanged ( newConfig );
+        super . onConfigurationChanged ( newConfig ) ;
 
-        if ( ( getResources ( ).getConfiguration ( ).uiMode & Configuration.UI_MODE_NIGHT_MASK ) == Configuration.UI_MODE_NIGHT_YES )
+        // الشرط الي جوا الاف معناه انه روح جيب الثيم الحالي و شوف اذا هو دارك ادخل و نفذ الي جوا الاف واعطي الخط تاع ال Radio buttons الوان الثيم الدارك اذا الثيم الحالي مش دارك اعطيهم الوان الثيم الفاتح
+        if ( ( getResources ( ) . getConfiguration ( ) . uiMode & Configuration . UI_MODE_NIGHT_MASK ) == Configuration . UI_MODE_NIGHT_YES )
         {
-            binding.DoctorRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_N ) );
-            binding.MomRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_N ) );
+            binding . DoctorRBTN2 . setTextColor ( getResources ( ) . getColor ( R . color . R_B_T_C_N ) ) ;
+            binding . MomRBTN2 . setTextColor ( getResources ( ) . getColor ( R . color . R_B_T_C_N ) ) ;
         }
         else
         {
-            binding.DoctorRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_L ) );
-            binding.MomRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_L ) );
+            binding . DoctorRBTN2 . setTextColor ( getResources ( ) . getColor ( R . color . R_B_T_C_L ) ) ;
+            binding . MomRBTN2 . setTextColor ( getResources ( ) . getColor ( R . color . R_B_T_C_L ) ) ;
         }
+    }
+
+    // هاد مربوط مع ايقونة التسجيل بواسطة قوقل
+    public void Sign_Up_By_Google ( View view )
+    {
+
+        // هون بقله اذا المسخدم مش محدد شو صفته ادخل الاف و عطل انشاء الحساب باستخدام قوقل اما اذا حدد فعادي خليه يستعمله
+        if ( !binding . MomRBTN2 . isChecked ( ) && !binding . DoctorRBTN2 . isChecked ( ) )
+        { Toast . makeText ( this , "يرجى تحديد صفتك قبل الانتقال الي  الصفحه التاليه" , Toast . LENGTH_LONG ) . show ( ) ; }
+        else
+        {
+            /*
+            binding  .  FacebookIcon       .  setEnabled ( true ) ;
+            binding  .  GoogleIcon         .  setEnabled ( true ) ;
+            */
+        }
+
+    }
+
+    // هاد مربوط مع ايقونة التسجيل بواسطة فيسبوك
+    public void Sign_Up_By_Facebook ( View view )
+    {
+
+        // هون بقله اذا المسخدم مش محدد شو صفته ادخل الاف و عطل انشاء الحساب باستخدام فيسبوك اما اذا حدد فعادي خليه يستعمله
+        if ( !binding . MomRBTN2 . isChecked ( ) && !binding . DoctorRBTN2 . isChecked ( ) )
+        { Toast . makeText ( this , "يرجى تحديد صفتك قبل الانتقال الي  الصفحه التاليه" , Toast . LENGTH_LONG ) . show ( ) ; }
+        else
+        {
+            /*
+            binding  .  FacebookIcon       .  setEnabled ( true ) ;
+            binding  .  GoogleIcon         .  setEnabled ( true ) ;
+            */
+        }
+
     }
 
 }
