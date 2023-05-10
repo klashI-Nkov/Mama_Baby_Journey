f32b46a27ae402fbc818c5180f72622d75dab128
klashinkov <klash1200@gmail.com>
2023-05-10 06:04:24 +0300

    By mohammed issa in 9/5/23 AT 10:59 PM

--- app/src/main/java/com/example/mamababyjourney/Create_An_Account_And_Sign_In/Sign_Up.java
+++ app/src/main/java/com/example/mamababyjourney/Create_An_Account_And_Sign_In/Sign_Up.java
@@ -1,7 +1,12 @@
 package com.example.mamababyjourney.Create_An_Account_And_Sign_In;
 
+import com.example.mamababyjourney.R;
 import com.example.mamababyjourney.databinding.ActivitySignUpBinding;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
 import android . view    . WindowManager;
 import java    . util    . Objects;
 import android . content . Intent;
@@ -28,6 +33,23 @@ public class Sign_Up extends AppCompatActivity
     {
         Intent intent = new Intent ( this , Info_Activity . class ) ;
         startActivity ( intent ) ;
+    }
+
+    @Override
+    public void onConfigurationChanged ( @NonNull Configuration newConfig )
+    {
+        super.onConfigurationChanged ( newConfig );
+
+        if ( ( getResources ( ).getConfiguration ( ).uiMode & Configuration.UI_MODE_NIGHT_MASK ) == Configuration.UI_MODE_NIGHT_YES )
+        {
+            binding.DoctorRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_N ) );
+            binding.MomRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_N ) );
+        }
+        else
+        {
+            binding.DoctorRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_L ) );
+            binding.MomRBTN2.setTextColor ( getResources ( ).getColor ( R.color.R_B_T_C_L ) );
+        }
     }
 
 }
